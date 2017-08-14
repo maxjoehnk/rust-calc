81d3d0f2b8df2b70b430342c0b5339c81e3de29c
Max Jöhnk <maxjoehnk@gmail.com>
2017-08-14 19:09:59 +0200

    [main] add basic calculation specs

--- src/main.rs
+++ src/main.rs
@@ -126,3 +126,89 @@ fn parse(input: String) -> Value {
     }
     panic!("Invalid Expression");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_primitive_returns_value() {
+        let val: f64 = 4.0;
+        let value = Value::Primitive(val);
+        assert_eq!(value.value(), val);
+    }
+
+    #[test]
+    fn value_expression_returns_result() {
+        let value = Value::Expression(Box::new(Expression {
+            a: Value::Primitive(2.0),
+            b: Value::Primitive(1.0),
+            action: Actions::ADD
+        }));
+        assert_eq!(value.value(), 3.0);
+    }
+
+    #[test]
+    fn expression_executes_addition() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Primitive(2.0),
+            action: Actions::ADD
+        };
+        assert_eq!(exp.eval(), 3.0);
+    }
+
+    #[test]
+    fn expression_executes_subtraction() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Primitive(2.0),
+            action: Actions::SUBTRACT
+        };
+        assert_eq!(exp.eval(), -1.0);
+    }
+
+    #[test]
+    fn expression_executes_multiplication() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Primitive(2.0),
+            action: Actions::MULTIPLY
+        };
+        assert_eq!(exp.eval(), 2.0);
+    }
+
+    #[test]
+    fn expression_executes_division() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Primitive(2.0),
+            action: Actions::DIVIDE
+        };
+        assert_eq!(exp.eval(), 0.5);
+    }
+
+    #[test]
+    fn expression_executes_exponentiation() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Primitive(2.0),
+            action: Actions::POWER
+        };
+        assert_eq!(exp.eval(), 1.0);
+    }
+
+    #[test]
+    fn expression_executes_nested_addition() {
+        let exp = Expression {
+            a: Value::Primitive(1.0),
+            b: Value::Expression(Box::new(Expression {
+                a: Value::Primitive(2.0),
+                b: Value::Primitive(3.0),
+                action: Actions::ADD
+            })),
+            action: Actions::ADD
+        };
+        assert_eq!(exp.eval(), 6.0);
+    }
+}
